f2711852ce6886ae8ef8d2a457acbae0a26d30f2
sergeysamohin <82103127+sergeysamohin@users.noreply.github.com>
2024-12-22 04:42:19 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
             </div>
             <div className="header-right">
                 <img
-                    src="../im.jpg"
+                    src="/public/im.jpg"
                     alt="Author"
                     className="header-photo"
                 />
